2e0aa8bd871d96b6fa40c15db7f5a0c5c3931e71
sheldon <sheldonmwangi254@gmail.com>
2023-06-19 10:54:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate") {
+    let fun;
+    return fun= `This Saturday, I want to ${activity}!`
+  }
+  const mondayWork = function (act="go to the office") {
+    let job;
+    
+    return  job=`This Monday, I will ${act}.`;
+  }
+
+function wrapAdjective( visual_flair= "*") {
+  const innerFunction = function ( character = "special") {
+    let res;
+    return  res=`You are ${visual_flair}${character}${visual_flair}!`
+  }
+  return innerFunction
+}
+ wrapAdjective(("%")("a dedicated programmer"));
